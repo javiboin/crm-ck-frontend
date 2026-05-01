e37e3251cef05be4986299d2d0bce46df9a9b152
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2026-04-30 23:36:13 -0300

    Update empty pages

---
+++ src/components/common/ErrorMessage.jsx
@@ -0,0 +1,9 @@
+const ErrorMessage = () => {
+    return (
+        <div>
+            <h1>Error Message</h1>
+        </div>
+    )
+}
+
+export default ErrorMessage
---
+++ src/components/common/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+const LoadingSpinner = () => {
+    return (
+        <div>
+            <h1>Loading Spinner</h1>
+        </div>
+    )
+}
+
+export default LoadingSpinner
