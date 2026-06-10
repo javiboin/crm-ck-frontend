61867bc22f96af75d68e234c09c4c3dcd706dc17
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2026-06-10 17:36:19 -0300

    refactor: Axios Port

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000'
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3001'
 })
 
 // interceptor para agregar el token en cada request
